90beb22e2774f21203531faec44d6fe75fcaabd4
sukanya <sukanyasirigiri@gmail.com>
2023-05-26 13:32:53 -0400

    name

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
   enable_dns_support = true
   enable_dns_hostnames = true
-  tags = merge(var.tags, { name = "${var.env}-vpc" })
+  tags = merge(var.tags, { Name = "${var.env}-vpc" })
 }
 
 module "subnets" {
