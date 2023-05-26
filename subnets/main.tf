08109c155a66e88e4d30fbc156c2cdddab792519
sukanya <sukanyasirigiri@gmail.com>
2023-05-26 12:21:53 -0400

    vars

---
+++ subnets/main.tf
@@ -0,0 +1,10 @@
+resource "aws_subnet" "main" {
+    count = length(var.cidr_block)
+  vpc_id     = var.vpc_id
+  cidr_block = var.cidr_block[count.index]
+
+  tags = merge(var.tags, {name = "${var.env}-${var.name}-${count.index}" })
+   
+    
+  
+}
--- subnets/vars.tf
+++ subnets/vars.tf
@@ -1,4 +1,6 @@
+variable "vpc_id" {}
 variable "cidr_block" {}
 variable "tags" {}
 variable "env" {}
-variable "subnets" {}
+variable "name" {}
+  
