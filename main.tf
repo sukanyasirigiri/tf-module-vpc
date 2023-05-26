22df3460be27fbffd77be149988b7d5d218add72
sukanya <sukanyasirigiri@gmail.com>
2023-05-26 13:53:55 -0400

    ip

--- main.tf
+++ main.tf
@@ -30,15 +30,15 @@ resource "aws_eip" "ngw" {
 
 }
 
-resource "aws_nat_gateway" "ngw" {
-  allocation_id = aws_eip.example.id
-  subnet_id     = aws_subnet.example.id
+#resource "aws_nat_gateway" "ngw" {
+#  allocation_id = aws_eip.example.id
+#  subnet_id     = aws_subnet.example.id
 
-  tags = {
-    Name = "gw NAT"
-  }
+  #tags = {
+   # Name = "gw NAT"
+  #}
 
   # To ensure proper ordering, it is recommended to add an explicit dependency
   # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.example]
-}
+  #depends_on = [aws_internet_gateway.example]
+#}
