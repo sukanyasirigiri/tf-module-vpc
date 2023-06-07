9e84564e1568c244bd20d09ae15bd1d4d6333227
sukanya <sukanyasirigiri@gmail.com>
2023-06-07 12:45:34 -0400

    peer

--- main.tf
+++ main.tf
@@ -61,3 +61,13 @@ output "ngw" {
     value = aws_nat_gateway.ngw
 }
 
+resource "aws_vpc_peering_connection" "peer" {
+  peer_owner_id = data.aws_caller_identity.identity.account_id
+  peer_vpc_id   = var.default_vpc_id
+  vpc_id        = aws_vpc.main.id
+  auto_accept = true
+
+  
+}
+
+
